2195ba6841b6242c2abacbd38614cdc1adea0d87
TaylorNg <kimtubin@gmail.com>
2021-09-07 22:21:05 +0700

    complete deleting todo function

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,23 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const initialState = {
-  todoList: [],
-}
-
 const todoSlice = createSlice({
   name: "todos",
-  initialState,
+  initialState: [],
   reducers: {
     saveTodo: (state, action) => {
-      state.todoList.push(action.payload)
+      state.push(action.payload)
     },
 
     deleteTodo: (state, action) => {
-      state.todoList.filter((todo) => todo.id !== action.payload.id)
+      return state.filter((todo) => todo.id !== action.payload.id)
     },
   },
 })
 
 export const { saveTodo, deleteTodo } = todoSlice.actions
-export const selectTodoList = (state) => state.todos.todoList
+export const selectTodoList = (state) => state.todos
 export default todoSlice.reducer
