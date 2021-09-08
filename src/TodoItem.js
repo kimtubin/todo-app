db0cb24e532262e2d851a284641551b60fa4e773
TaylorNg <kimtubin@gmail.com>
2021-09-08 21:56:39 +0700

    not include README file

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,8 +3,10 @@ import EditIcon from "@material-ui/icons/Edit"
 import HighlightOffIcon from "@material-ui/icons/HighlightOff"
 import { deleteTodo } from "./features/todoSlice"
 import { useDispatch } from "react-redux"
+import { useState } from "react"
 
 export default function TodoItem({ name, id }) {
+  const [editable, setEditable] = useState(false)
   const dispatch = useDispatch()
   const handleDelete = () => {
     dispatch(
@@ -15,8 +17,8 @@ export default function TodoItem({ name, id }) {
   }
   return (
     <div className="todo-item">
-      <p className="todo-content">{name}</p>
-      <EditIcon className="icon" />
+      {editable ? <input type="text" placeholder={name} /> : <p className="todo-content">{name}</p>}
+      <EditIcon className="icon" onClick={() => setEditable(!editable)} />
       <HighlightOffIcon className="icon" onClick={handleDelete} />
     </div>
   )
